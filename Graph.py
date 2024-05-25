65bf8861dedee94fc339f75b1c366b9b6e2a24a4
ワン <102137836+rdwn354@users.noreply.github.com>
2024-05-25 15:07:08 +0700

    update decision

--- Graph.py
+++ Graph.py
@@ -45,12 +45,8 @@ class BlinkAnalyzer:
                 value.append(values)
                 print(f"Row {row_index} not found or contains only zeros.")
 
-
         print(blink)
-        print(value)
-
         time_blink_1d = [time for sublist in value for time in sublist]
-
         return time_blink_1d, blink, value
 
 
@@ -65,28 +61,24 @@ class ShowGraph:
         screen_height_in = screen_height_px / desired_dpi
 
         fig = plt.figure(figsize=(screen_width_in, screen_height_in), dpi=desired_dpi)
-        # creating tren
         x = np.arange(len(blink))
         y = np.array(blink)
         slope, intercept = np.polyfit(x, y, 1)
-
-        # Menentukan arah tren
         if slope > 0:
             trend_direction = "up"
         elif slope < 0:
             trend_direction = "down"
         else:
             trend_direction = "flat"
-
         treadline = intercept + slope * x
 
-        # Plot for blink counts
         plt.subplot(1, 2, 1)
         plt.plot(blink)
-        plt.plot(treadline, label=f'Garis Tren {trend_direction}', color='red', linewidth = 1)
+        plt.plot(treadline, label=f'Garis Tren {trend_direction}', color='red', linewidth=1)
         plt.xlabel('Time (minute)')
         plt.ylabel('Blink')
         plt.title('Blink Count')
+        plt.axhline(y=10, color='r', linestyle='--', linewidth=1)
 
         lowest_value = min(blink)
         lowest_index = np.argmin(blink)
@@ -98,20 +90,25 @@ class ShowGraph:
         values_under_10 = len([x for x in blink if x <= 10])
 
         status = values_under_10 >= median_blink
+        if not status:
+            if trend_direction == "up" and trend_direction == "down":
+                diagnose = "Normal"
+        if status:
+            if trend_direction == "up":
+                diagnose = "Normal"
+            else:
+                diagnose = "Computer Vision Syndrome"
+        else:
+            diagnose = "Normal"
 
-        # Printing the indices (if needed)
-        if lowest_index != -1:
-            print("Lowest element index: ", lowest_index)
-        if highest_index != -1:
-            print("Highest element index: ", highest_index)
-
+        print(status)
+        print(trend_direction)
         plt.subplot(1, 2, 2)
-        plt.text(0, 0.9, f'Name: {csv_file}', fontsize=8)
         plt.text(0, 0.8, f'Total time : {len(blink)} minutes', fontsize=8)
         plt.text(0, 0.7, f'Blink under set point : {values_under_10} times', fontsize=8)
         plt.text(0, 0.6, f'Lowest blink in minute {lowest_index} with {lowest_value} blinks', fontsize=8)
         plt.text(0, 0.5, f'Highest blink in minute {highest_index} with {highest_value} blinks', fontsize=8)
-        plt.text(0, 0.4, f'Indicate : {"Computer Vision Syndrome" if trend_direction == "down" else "Normal"}', fontsize=8)
+        plt.text(0, 0.4, f'Status : {diagnose}', fontsize=8)
 
         plt.xlim(0, 1)
         plt.ylim(0, 1)
@@ -119,13 +116,9 @@ class ShowGraph:
 
         plt.tight_layout()
 
-        # Display the plot
         plt.show()
 
 
 analyzer = BlinkAnalyzer(path_csv)
-
 graph_display = ShowGraph()
-
-# Call plot_data method to display the graph for the given time
 graph_display.plot_data(analyzer, time)
